eceada6505642a8837b7a3dd850863986f2401dd
petra kohler <kohler@brickwork.ch>
2021-03-16 13:14:28 +0100

    add. test for method references

--- src/main/java/oop2/module04/MethodReferences.java
+++ src/main/java/oop2/module04/MethodReferences.java
@@ -1,5 +1,6 @@
 package oop2.module04;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
@@ -10,22 +11,59 @@ import java.util.function.Supplier;
  */
 public class MethodReferences {
     public static void main(String[] args) {
+
+        // test für externe Methode
+        String myExString = "25";
+        int exNumber = stringToInteger(myExString);
+        System.out.println(exNumber); // 25
+
+        // -------------------------
+
+        String myString = "42";
         Function<String, Integer> stringToInteger = (String s) -> Integer.parseInt(s);
 
+        int number = stringToInteger.apply(myString);
+        System.out.println(number); //42
+
+        // -------------------------
+
+        List<String> myList = Arrays.asList("Hallo", "Grüezi", "Hola");
+
         BiPredicate<List<String>, String> contains = (list, element) -> list.contains(element);
+        System.out.println(contains.test(myList, "Hallo")); // true
+        System.out.println(contains.test(myList, "Hoi")); // false
 
-        Function<Integer, String> func = (Integer i) -> {
-            return "James" + i;
-        };
+        // -------------------------
 
-        Supplier<String> func2 = () -> "Raoul";
+        // Externe Methode
+        System.out.println(myBiPredicate(myList, "Hallo", (list, e) -> list.size() > 2));
 
-        Supplier<String> func3 = () -> {
-            return "Richard";
-        };
 
-//		Consumer<String> func4 = (String s) -> {
-//			return "Raoul";
-//		};
+
+
+
+//        Function<Integer, String> func = (Integer i) -> {
+//            return "James" + i;
+//        };
+//
+//        Supplier<String> func2 = () -> "Raoul";
+//
+//        Supplier<String> func3 = () -> {
+//            return "Richard";
+//        };
+
+//		  Consumer<String> func4 = (String s) -> {
+//		  return "Raoul";
+
     }
+
+    // Als eigene Methode für eine spezielle Aufgabe
+    public static int stringToInteger(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static boolean myBiPredicate(List<String> list, String value, BiPredicate<List<String>, String> expression) {
+        return expression.test(list, value);
+    };
+
 }
